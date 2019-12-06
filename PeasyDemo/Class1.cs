4f09d090d5463ebd4c91c155da21a10ddcaa6623
Eugene M <ujeen1980@gmail.com>
2019-12-06 14:58:44 +0200

    PeasyDemo start

--- PeasyDemo/Class1.cs
+++ PeasyDemo/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Peasy;
 
 namespace PeasyDemo
 {
-    public class Class1
+    public class Person : IDomainObject<int>
     {
+        public int ID { get; set; }
     }
 }
