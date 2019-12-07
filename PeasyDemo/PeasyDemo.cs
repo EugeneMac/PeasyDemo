d79a37c3b88cfb2f3a1ef768bcecb51a4650b493
Eugene M <ujeen1980@gmail.com>
2019-12-07 12:28:51 +0200

    Person class complete

---
+++ PeasyDemo/PeasyDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peasy;
+
+namespace PeasyDemo
+{
+    public class Person : IDomainObject<int>
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public DateTime birthDate { get; set; }
+        public string SSN { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+}
